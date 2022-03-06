7a8f633c74229c0b9e3e62d79976b254d3383f1d
takaya <s.takaya@librus.jp>
2022-03-06 13:32:15 +0900

    機能をクラスに分割

--- test/test.py
+++ test/test.py
@@ -1,4 +1,30 @@
 # coding: utf-8
-print("foo")
-print("bar")
-print("ふがふが")
+import numpy as np
+import matplotlib.pyplot as plt
+
+class PlotGlaph:
+    def show_plot():
+        print("a")
+
+class RK4:
+    def init():
+        # t-時間, n-分割数, h-一区間の幅, グラフのx軸, f-方程式
+        t = 50  
+        n = 1000
+        h = t/n
+        x_axis = np.arange(0, t, h)
+        f = 1
+    
+    def calculate():
+        return 0
+
+# インスタンス作成
+rk4 = RK4()
+plotglaph = PlotGlaph()
+
+# 計算
+rk4.init()
+data = rk4.calculate()
+
+# 出力
+PlotGlaph(data)
