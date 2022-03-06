6688f4cdeb2b8966bedb19de69c5b99bc6af3a72
takaya <s.takaya@librus.jp>
2022-03-06 14:15:37 +0900

    [test] : 誤差計算　完成

--- test/test.py
+++ test/test.py
@@ -20,23 +20,37 @@ class RK4:
         # 結果用の配列
         self.u = np.empty(self.n)
         self.u[0] = 1.0
+
+        # 計算比較用の真値配列
+        self.v = np.empty(self.n)
+        for i in range(self.n):
+            self.v[i] = np.exp(self.h*i)
     
-    def calculate(self):
+    def calculate_rk4(self):
         for i in range (self.n-1):
             k_1 = self.h * self.f(self.u[i], self.t[i])
             k_2 = self.h * self.f(self.u[i] + k_1/2.0, self.t[i] + self.h/2.0)
             k_3 = self.h * self.f(self.u[i] + k_2/2.0, self.t[i] + self.h/2.0)
             k_4 = self.h * self.f(self.u[i] + k_3, self.t[i])
             self.u[i+1] = self.u[i] + 1/6 * (k_1 + 2.0*k_2 + 2.0*k_3 + k_4)
+        
         return self.u
+    
+    def calculate_error(self):
+        e = self.v-self.u
+        return e
 
 # インスタンス作成
 rk4 = RK4()
 plotglaph = PlotGlaph()
 
-# 計算
-data = rk4.calculate()
+# 推定値　計算
+data = rk4.calculate_rk4()
+print("rk4.calculate_rk4() finished")
 
-print (data)
+# 誤差　計算
+error = rk4.calculate_error()
+print("rk4.calculate_error() finished")
+
 # 出力
 #PlotGlaph(data)
