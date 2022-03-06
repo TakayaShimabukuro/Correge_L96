0e520a6544a801d2895eb6c9bf3539355d157f56
takaya <s.takaya@librus.jp>
2022-03-06 14:05:07 +0900

    ルンゲクッタ法の計算に成功

--- test/test.py
+++ test/test.py
@@ -7,24 +7,36 @@ class PlotGlaph:
         print("a")
 
 class RK4:
-    def init():
-        # t-時間, n-分割数, h-一区間の幅, グラフのx軸, f-方程式
-        t = 50  
-        n = 1000
-        h = t/n
-        x_axis = np.arange(0, t, h)
-        f = 1
+    def __init__(self):
+        # T-時間, n-分割数, h-一区間の幅, t-グラフのx軸
+        self.T = 50
+        self.n = 1000
+        self.h = self.T/self.n
+        self.t = np.arange(0, self.T, self.h)
+        
+        # f-方程式 ( 引数1, 引数2 : 式)、引数は指定がない場合初期値をいれられる。(t=0)
+        self.f = lambda u, t=0 : u 
+
+        # 結果用の配列
+        self.u = np.empty(self.n)
+        self.u[0] = 1.0
     
-    def calculate():
-        return 0
+    def calculate(self):
+        for i in range (self.n-1):
+            k_1 = self.h * self.f(self.u[i], self.t[i])
+            k_2 = self.h * self.f(self.u[i] + k_1/2.0, self.t[i] + self.h/2.0)
+            k_3 = self.h * self.f(self.u[i] + k_2/2.0, self.t[i] + self.h/2.0)
+            k_4 = self.h * self.f(self.u[i] + k_3, self.t[i])
+            self.u[i+1] = self.u[i] + 1/6 * (k_1 + 2.0*k_2 + 2.0*k_3 + k_4)
+        return self.u
 
 # インスタンス作成
 rk4 = RK4()
 plotglaph = PlotGlaph()
 
 # 計算
-rk4.init()
 data = rk4.calculate()
 
+print (data)
 # 出力
-PlotGlaph(data)
+#PlotGlaph(data)
