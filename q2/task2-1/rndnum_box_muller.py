9aab3d67c62812abcfd6604bfbe27687fa469ab5
takaya <s.takaya@librus.jp>
2022-03-08 20:46:25 +0900

    観測値導出メソッドを追加

--- q2/task2-1/rndnum_box_muller.py
+++ q2/task2-1/rndnum_box_muller.py
@@ -3,16 +3,15 @@ import random
 import sys
 import traceback
 
-
 class RndnumBoxMuller:
-    M = 10        # 平均
-    S = 2.5       # 標準偏差
-    N = 10000     # 生成する個数
+    M     = 20        # 平均
+    S     = 2.5       # 標準偏差
+    N     = 100     # 生成する個数
     SCALE = N // 100  # ヒストグラムのスケール
 
     def __init__(self):
         self.hist = [0 for _ in range(self.M * 5)]
-
+    
     def generate_rndnum(self):
         try:
             for _ in range(self.N):
@@ -20,26 +19,36 @@ class RndnumBoxMuller:
                 self.hist[res[0]] += 1
                 self.hist[res[1]] += 1
                 
-            print(len(self.hist))
         except Exception as e:
             raise
-
+    
     def __rnd(self):
         try:
             r_1 = random.random()
             r_2 = random.random()
             x = self.S \
-                * math.sqrt(-2 * math.log(r_1)) \
-                * math.cos(2 * math.pi * r_2) \
+              * math.sqrt(-2 * math.log(r_1)) \
+              * math.cos(2 * math.pi * r_2) \
                 + self.M
             y = self.S \
-                * math.sqrt(-2 * math.log(r_1)) \
-                * math.sin(2 * math.pi * r_2) \
+              * math.sqrt(-2 * math.log(r_1)) \
+              * math.sin(2 * math.pi * r_2) \
                 + self.M
+            print(x)
             return [math.floor(x), math.floor(y)]
         except Exception as e:
             raise
-
+    def display(self):
+        """ Display """
+        try:
+            for i in range(0, self.M * 2 + 1):
+                print("{:>3}:{:>4} | ".format(i, self.hist[i]), end="")
+                for j in range(1, self.hist[i] // self.SCALE + 1):
+                    print("*", end="")
+                print()
+        except Exception as e:
+            raise
 
 box_muller = RndnumBoxMuller()
 box_muller.generate_rndnum()
+box_muller.display()
--- q2/task2-1/task2-1.py
+++ q2/task2-1/task2-1.py
@@ -12,19 +12,32 @@ from sklearn.metrics import mean_squared_error
 F = 8.0
 DT = 0.05
 N = 40
-DAYS = 1000 #21900
+DAYS = 1000.0  # 21900
+print(type(DAYS))
+
 
 class RndnumBoxMuller:
-    M = 1        # 平均
-    S = 2.5       # 標準偏差
-    N = 10000     # 生成する個数
-    SCALE = N // 100  # ヒストグラムのスケール
+    M = 20      # 平均
+    S = 2.5    # 標準偏差
+    N = 40     # 生成する個数
     std = 0.001
     n_round = 3
 
+    def __init__(self, X):
+        self.hist = X
 
     def generate_rndnum(self):
-        # ガウス分布に従う乱数をBox muller法で作成。式は以下。
+        try:
+            for _ in range(self.N):
+                res = self.__rnd()
+                self.hist[res[0]] += self.std
+                self.hist[res[1]] += self.std
+
+            return self.hist
+        except Exception as e:
+            raise
+
+    def __rnd(self):
         try:
             r_1 = random.random()
             r_2 = random.random()
@@ -36,34 +49,32 @@ class RndnumBoxMuller:
                 * math.sqrt(-2 * math.log(r_1)) \
                 * math.sin(2 * math.pi * r_2) \
                 + self.M
-            
-            x = round(x*self.std, self.n_round)
-            y = round(y*self.std, self.n_round)
-            return [x, y]
+            print(x)
+            return [math.floor(x), math.floor(y)]
         except Exception as e:
             raise
 
 
 class Model_L96:
     def __init__(self, N, DAYS, F, DT):
-        
+
         self.N = N
         self.F = F
         self.dt = DT
         self.DAYS = DAYS
 
-        self.Xn = np.zeros((self.N, self.DAYS))
-        self.t = np.zeros((self.DAYS))
-    
+        self.Xn = np.zeros((self.N, int(self.DAYS)))
+        self.t = np.zeros(int(self.DAYS))
+
     def f_l96(self, x):
         f = np.zeros((self.N))
         for i in range(2, self.N-1):
             f[i] = (x[i+1]-x[i-2])*x[i-1]-x[i]+self.F
-        
+
         f[0] = (x[1]-x[self.N-2])*x[self.N-1]-x[0]+self.F
         f[1] = (x[2]-x[self.N-1])*x[0]-x[1]+self.F
         f[self.N-1] = (x[0]-x[self.N-3])*x[self.N-2]-x[self.N-1]+self.F
-        
+
         return f
 
     def cal_RK4(self, X_old):
@@ -73,7 +84,7 @@ class Model_L96:
         k4 = self.f_l96(X_old + k3*self.dt)
         X_new = X_old + self.dt/6.0 * (k1 + 2.0*k2 + 2.0*k3 + k4)
         return X_new
-    
+
     def get_estimated_data(self):
         # 初期値X_iのotherwiseを生成
         X = float(self.F)*np.ones(self.N)
@@ -83,31 +94,53 @@ class Model_L96:
 
         # Xnの初期値を代入
         for i in range(self.N):
-            self.Xn[i, 0] = copy(X[i]) 
-        
+            self.Xn[i, 0] = copy(X[i])
+
         # Xnの二回目以降を計算
-        for j in range(1, self.DAYS):
+        for j in range(1, int(self.DAYS)):
             X = self.cal_RK4(X)
             self.Xn[:, j] = X[:]
             self.t[j] = self.dt*j
-        
+
         return self.Xn
 
+    def get_estimated_data_added_noise(self, xn_true):
+        xn_pred = np.zeros((self.N, int(self.DAYS)))
+        # 乱数を付加
+        for i in range(self.N):
+            box_muller = RndnumBoxMuller(xn_true)
+            xn_pred[i, 0] = box_muller.generate_rndnum()
+
+        # Xnの二回目以降を計算
+        for j in range(1, self.N):
+            box_muller = RndnumBoxMuller(xn_true)
+            xn_true = self.cal_RK4(box_muller.generate_rndnum())
+            self.Xn[:, j] = xn_true[:]
+            self.t[j] = self.dt*j
+
+        return self.Xn
+
+
 def show_graph(xn):
-    for i in range(0, DAYS):
+    for i in range(0, int(DAYS)):
         print(xn[:, i])
-        if i == DAYS-1:
+        if i == int(DAYS)-1:
             fig = plt.figure()
             plt.plot(xn[:, i])
             plt.xlim(0, 40)
             plt.show()
 
 
-# インスタンス作成
+# L96 - 真値導出
 l96_rk4 = Model_L96(N, DAYS, F, DT)
 xn = l96_rk4.get_estimated_data()
-xn_true = xn[:, DAYS-1]
-print(xn_true)
+xn_true = xn[:, int(DAYS)-1]
+#print(xn)
+
+# L96 - 観測値導出
+l96_rk4_true = Model_L96(N, F, int(DAYS), DT)
+#xn_pred = l96_rk4_true.get_estimated_data_added_noise(xn_true)
+# print(xn_pred)
 
 # 出力
 show_graph(xn)
