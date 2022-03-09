f4350f34a5d20983dc36fe82f027c8b9818433a3
takaya <s.takaya@librus.jp>
2022-03-09 09:57:06 +0900

    rmseより前の動作確認に成功

--- q2/task2-1/task2-1.py
+++ q2/task2-1/task2-1.py
@@ -7,6 +7,7 @@ import math
 import random
 from sklearn.metrics import mean_squared_error
 
+
 class Model_L96:
     def __init__(self):
 
@@ -19,13 +20,9 @@ class Model_L96:
 
         self.Xn = np.zeros((self.N, self.DAYS))
         self.Xn_true = np.zeros((self.N, self.STEP))
+        self.Xn_pred = np.zeros((self.N, self.DAYS2))
         self.Xn_noise = []
-        self.Xn_pred = np.zeros((self.DAYS2, self.N, self.DAYS2))
-        self.t = np.zeros(self.DAYS)
-        self.t_true = np.zeros(self.DAYS)
-        self.t_pred = np.zeros((self.DAYS2, self.DAYS))
-
-        self.std = 0.01
+        self.std = 0.001
 
     def f_l96(self, x):
         f = np.zeros((self.N))
@@ -49,52 +46,61 @@ class Model_L96:
     def get_estimated_data(self):
         X = float(self.F)*np.ones(self.N)
         X[20] = 1.001*self.F
+        self.Xn = self.create_table(self.N, self.DAYS, X, self.Xn)
 
-        for i in range(self.N):
-            self.Xn[i, 0] = copy(X[i])
-
-        for j in range(1, int(self.DAYS)):
-            X = self.cal_RK4(X)
-            self.Xn[:, j] = X[:]
-            self.t[j] = self.dt*j
-    
     def get_estimated_data_true(self):
-        X = self.Xn[:, int(self.DAYS)-1]
-        for i in range(self.N):
-            self.Xn_true[i, 0] = copy(X[i])
-
-        for j in range(1, int(self.STEP)):
-            X = self.cal_RK4(X)
-            self.Xn_true[:, j] = X[:]
-            self.t_true[j] = self.dt*j
+        X = self.Xn[:, self.DAYS-1]
+        self.Xn_true = self.create_table(self.N, self.STEP, X, self.Xn_true)
 
     def get_estimated_data_pred(self):
+        tmp = []
         for i in range(self.DAYS2):
             X = self.Xn_noise[i]
-            for j in range(self.N):
-                self.Xn_pred[i][j, 0] = copy(X[j])
+            self.Xn_pred = self.create_table(self.N, self.DAYS2, X, self.Xn_pred)
+            tmp.append(self.Xn_pred)
+        return tmp
 
-            for k in range(1, int(self.DAYS2)):
-                X = self.cal_RK4(X)
-                self.Xn_pred[i][:, k] = X[:]
-                self.t_pred[i][k] = self.dt*k
-    
     def add_noise(self):
         noise = np.random.normal(loc=0, scale=1, size=40)
         tmp = []
-        for i in range(0, int(self.DAYS2)):
+        for i in range(0, self.DAYS2):
             tmp = self.Xn_true[:, i] + noise
             self.Xn_noise.append(tmp)
-    
-    def get_rsme(self):
-        tmp = []
-        for i in range(self.DAYS2):
-            tmp.append(self.Xn_true[:, i:i+self.DAYS2] - self.Xn_pred[i])
-        
-        tmp = np.array(tmp)
-        rmse = np.mean(np.sqrt(np.mean(np.power(tmp, 2), axis = 2)), axis = 0)
 
-        return rmse
+    def get_rsme(self, data_pred):
+        rmse_tmp = []
+        for i in range(self.DAYS2):
+            show_graph(self.Xn_true[:, i:i+self.DAYS2], i)
+            tmp = np.sqrt(mean_squared_error(self.Xn_true[:, i:i+self.DAYS2], data_pred[i][:, :]))
+            rmse_tmp.append(tmp)
+            
+        return rmse_tmp
+
+    def create_table(self, n, step, X, tmp_X):
+        for i in range(n):
+            tmp_X[i, 0] = copy(X[i])
+        for j in range(1, step):
+            X = self.cal_RK4(X)
+            tmp_X[:, j] = X[:]
+        return tmp_X
+
+    def show_graph(self, rsme, i):
+        fig = plt.figure()
+        try:
+            new_dir_path = './q2/' + str(i)
+            os.mkdir(new_dir_path)
+        except FileExistsError:
+            pass
+        for j in range(0, self.DAYS2):
+            if j%10 == 0:
+                plt.plot(rsme[:, j])
+                plt.xlabel("time(days)")
+                plt.ylabel("RSME")
+                #plt.xticks(np.arange(0, 110, step=10))
+                plt.grid(color='k', linestyle='dotted', linewidth=0.5)
+                name =  "./q2/" + str(i) + "/" + str(j) + ".png"
+                plt.savefig(name)
+                plt.close()
 
 def show_graph(rsme):
     fig = plt.figure()
@@ -102,29 +108,32 @@ def show_graph(rsme):
     plt.xlabel("time(days)")
     plt.ylabel("RSME")
     plt.legend()
-    plt.xticks(np.arange(0, 110, step=10))
+    #plt.xticks(np.arange(0, 110, step=10))
     plt.grid(color='k', linestyle='dotted', linewidth=0.5)
-    plt.savefig("result.png")
+    plt.savefig("./q2/result.png")
     plt.close()
-
 
 
 l96_rk4 = Model_L96()
 
-# 1.3年分のシミュレーション
-l96_rk4.get_estimated_data() 
+# 1.3年分のシミュレーション 40 (element) * 5000 (time)
+l96_rk4.get_estimated_data()
 
-# 2.3年分からスタートして300ステップの真値を取得
+# 2.3年分からスタートして300ステップの真値を取得 40 (element) * 300 (time)
 l96_rk4.get_estimated_data_true()
+# print(len(l96_rk4.Xn_true[0]))
+# print(l96_rk4.Xn_true)
 
-# 3.真値の各ステップにノイズを付加。
+# 3.真値の各ステップにノイズを付加。 100 (time) * 40 (element) * 1 (time)
 l96_rk4.add_noise()
 
 # 4.ノイズ付加された配列を100STEP時間を進める。
-l96_rk4.get_estimated_data_pred()
+data_pred = l96_rk4.get_estimated_data_pred()
+#print(len(data_pred[0]))
+#print(data_pred)
 
 # RSME 計算
-rmse = l96_rk4.get_rsme()
-
+rmse = l96_rk4.get_rsme(data_pred)
+print(len(rmse))
 # 出力
-show_graph(rmse)
+#show_graph(rmse)
