9335138e11bc2accb05f3f65dd8e0085ce69169b
takaya <s.takaya@librus.jp>
2022-03-13 19:13:25 +0900

    q2の完成

--- q2/main.py
+++ q2/main.py
@@ -25,66 +25,80 @@ basicConfig(filename='console.log', level=DEBUG, filemode='w')
 N = 41
 F = 8.0
 dt = 0.05
-std = 0.001
+std_list = [0.001, 0.01, 0.1, 1.0]
 
 logger.info('Prosess Start!!')
-# 3年分のシミュレーションを行い、真値の初期値を作成する。
-logger.info('Prosess 1')
-l96 = Model_L96(N, F, dt, std)
+for k in range(len(std_list)):
+    # 3年分のシミュレーションを行い、真値の初期値を作成する。
+    logger.info('Prosess 1')
+    l96 = Model_L96(N, F, dt, std_list[k])
 
-n_step_init = 5000 
-Xn_init = np.zeros((N, n_step_init))
-X1_init= float(F)*np.ones(N)
-X1_init[20] = 1.001*F
+    n_step_init = 5000 
+    Xn_init = np.zeros((N, n_step_init))
+    X1_init= float(F)*np.ones(N)
+    X1_init[20] = 1.001*F
 
-Xn_init, tn_init= l96.analyze_model(Xn_init, X1_init, n_step_init)
-
-
-# 初期値Xnをもとに真値Xn[40, :]を取得
-logger.info('Prosess 2')
-
-n_step_true = 4000
-Xn_true = np.zeros((N, n_step_true))
-X1_true = Xn_init[:, n_step_init-1]
-
-Xn_true, tn_true = l96.analyze_model(Xn_true, X1_true, n_step_true)
+    Xn_init, tn_init= l96.analyze_model(Xn_init, X1_init, n_step_init)
 
 
-# 真値の1-1000stepをそれぞれ初期値とし,　そこにノイズを加え、時間経過
-logger.info('Prosess 3')
+    # 初期値Xnをもとに真値Xn[40, :]を取得
+    logger.info('Prosess 2')
 
-n_step_forcast_init = 1000
-n_step_forcast = 9
-Xn_forcasts = []
-for i in range(n_step_forcast_init):
-    Xn_forcast = np.zeros((N, n_step_forcast))
-    X1_forcast = l96.add_noise(Xn_true[:, i])
+    n_step_true = 4000
+    Xn_true = np.zeros((N, n_step_true))
+    X1_true = Xn_init[:, n_step_init-1]
 
-    Xn_forcast, tn_forcast = l96.analyze_model(Xn_forcast, X1_forcast, n_step_forcast)
-    Xn_forcasts.append(Xn_forcast)
+    Xn_true, tn_true = l96.analyze_model(Xn_true, X1_true, n_step_true)
 
 
-# Xn_truesをスライス
-logger.info('Prosess 4')
-Xn_trues = []
-for i in range(n_step_forcast_init):
-    Xn_trues.append(Xn_true[:, i:i+n_step_forcast])
+    # 真値の1-1000stepをそれぞれ初期値とし,　そこにノイズを加え、時間経過
+    logger.info('Prosess 3')
+
+    n_step_forcast_init = 1000
+    n_step_forcast = 200 # 400step - 100day
+    Xn_forcasts = []
+    for i in range(n_step_forcast_init):
+        Xn_forcast = np.zeros((N, n_step_forcast))
+        X1_forcast = l96.add_noise(Xn_true[:, i])
+
+        Xn_forcast, tn_forcast = l96.analyze_model(Xn_forcast, X1_forcast, n_step_forcast)
+        Xn_forcasts.append(Xn_forcast)
 
 
-# rmseを導出する。
-logger.info('Prosess 5')
-#rmse = l96.get_RMSE(Xn_trues, Xn_forcasts, n_step_forcast)
+    # Xn_truesをスライス
+    logger.info('Prosess 4')
+    Xn_trues = []
+    for i in range(n_step_forcast_init):
+        Xn_trues.append(Xn_true[:, i:i+n_step_forcast])
 
-num_list = np.arange(0, n_step_forcast_init, step=100)
-rmse_list = []
-for i in range(len(num_list)):
-    rmse_list.append(l96.get_RMSE_solo(Xn_trues, Xn_forcasts, n_step_forcast, num_list[i]))
 
-# グラフ出力
-logger.info('Prosess 6')
-file_path = "./q2/result/"
-plot = Plot_Methods()
-for i in range(len(num_list)):
-    plot.xy_graph_rmse_list(rmse_list, file_path, tn_forcast, std, "list")
+    # rmseを導出する。
+    logger.info('Prosess 5')
+    num_list = np.arange(0, 1000, step=1)
+    rmse_sum = [0] * 1000
+
+    for i in range(len(num_list)):
+        # すべてのシミュレーション結果の合計を求める
+        rmse = l96.get_RMSE_solo(Xn_trues, Xn_forcasts, n_step_forcast, num_list[i])
+        rmse_sum = list(map(lambda val1, val2: val1 + val2, rmse_sum, rmse))
+
+    # nで割って平均を求める
+    rmse_sum = list(map(lambda val: val / len(num_list), rmse_sum))
+    logger.debug("---------")
+    logger.debug(std_list[k])
+    logger.debug(rmse_sum[0])
+    logger.debug(rmse_sum[3])
+    logger.debug(rmse_sum[4])
+    logger.debug(rmse_sum[7])
+    logger.debug(rmse_sum[8])
+
+
+    # グラフ出力
+    logger.info('Prosess 6')
+    file_path = "./q2/result/"
+    plot = Plot_Methods()
+    #plot.xy_graph_rmse(rmse_sum, file_path, tn_forcast, std_list[k])
+    plot.xy_graph_rmse_log(rmse_sum, file_path, tn_forcast, std_list[k])
+
 
 logger.info('Prosess finish')
--- q2/plot.py
+++ q2/plot.py
@@ -1,9 +1,6 @@
 import matplotlib.pyplot as plt
-from logging import getLogger
 import os
 import numpy as np
-logger = getLogger(__name__)
-
 
 class Plot_Methods:
     def make_file(self, file_path):
@@ -12,69 +9,32 @@ class Plot_Methods:
         except FileExistsError:
             pass
 
-    def xy_graph_l96(self, Xn, step, file_path):
-        fig = plt.figure()
-        self.make_file(file_path)
-
-        x_length = np.arange(0, len(Xn), step=1)
-        for i in range(0, step):
-            if i % 10 == 0:
-                plt.plot(x_length, Xn[:, i])
-                plt.grid(color='k', linestyle='dotted', linewidth=0.5)
-                file_name = file_path + str(i) + ".png"
-                plt.savefig(file_name)
-                plt.close()
-
-    def xy_graph_hovmoller(self, Xn_list, file_path):
-
-        self.make_file(file_path)
-        F_list = [0.1, 0.5, 1.0, 3.0, 5.0, 8.0]  # task1-3専用
-        x_length = np.arange(0, len(Xn_list[0]), step=1)
-
-        dt = 0.05
-        t = []
-        for j in range(0, len(Xn_list[0])):
-            t.append(dt*j)
-        
-        logger.debug(t)
-        for i, val in enumerate(F_list):
-            fig, ax = plt.subplots(1, 1)
-            image1 = ax.contourf(
-                Xn_list[i][:, :], t, cmap='gist_rainbow_r',  levels=10)
-            cb1 = fig.colorbar(image1, ax=ax,  orientation='horizontal')
-            file_name = file_path + str(val) + ".png"
-            plt.savefig(file_name)
-            plt.close()
-
     def xy_graph_rmse(self, rmse, file_path, tn_forcast, std):
         fig = plt.figure()
         self.make_file(file_path)
 
-        x_length = np.arange(0, len(rmse), step=0.25)
-        plt.xticks(np.arange(0, 2.25, step=0.25))
-        plt.plot(tn_forcast[:], rmse)
-        logger.debug(tn_forcast)
-        logger.debug(rmse)
-    
+        plt.xticks(np.arange(0, 101, step=10))
+        plt.plot(tn_forcast, rmse)
         plt.grid(color='k', linestyle='dotted', linewidth=0.5)
-        file_name = file_path + str(std) + ".png"
+        file_name = file_path + str(std) + "-100days" + ".png"
         plt.savefig(file_name)
         plt.close()
     
-    def xy_graph_rmse_list(self, rmse, file_path, tn_forcast, std, num):
+    
+    def xy_graph_rmse_log(self, rmse, file_path, tn_forcast, std):
         fig = plt.figure()
         self.make_file(file_path)
+        ax1 = fig.subplots()
+        ax2 = ax1.twinx()
 
-        x_length = np.arange(0, len(rmse), step=0.25)
-        plt.xticks(np.arange(0, 2.25, step=0.25))
-
-        for i, val in enumerate(rmse):
-            plt.plot(tn_forcast[:], rmse[i])
-            logger.debug(tn_forcast)
-            logger.debug(rmse)
+        ax1.plot(tn_forcast, rmse)
+        plt.xticks(np.arange(0, 101, step=10))
+        
+        ax2.plot(tn_forcast, rmse)
+        ax2.set_yscale('log')
         
         plt.grid(color='k', linestyle='dotted', linewidth=0.5)
-        file_name = file_path + str(std) + "-" + num + ".png"
+
+        file_name = file_path + str(std) + "-50days" + ".png"
         plt.savefig(file_name)
         plt.close()
-
