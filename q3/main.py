315de527f2e4bea5640de773ff5ceebe37697a26
takaya <s.takaya@librus.jp>
2022-03-15 22:02:25 +0900

    [q3] process 3, 4 OK

--- q3/main.py
+++ q3/main.py
@@ -16,42 +16,58 @@ logger.error('error')
 logger.info('Process End!')
 '''
 
-logger = getLogger(__name__)                                                            # logの初期設定
+# logの初期設定
+logger = getLogger(__name__)
 logger.setLevel(DEBUG)
 basicConfig(filename='console.log', level=DEBUG, filemode='w')
 
 
-N = 41                                                                                  # public parameter
+# public parameter
+N = 41
 F = 8.0
 dt = 0.05
-std_list = [0.001]
+std = 0.001
+mu = 0
+sigma = 1
+
+# local parameter
+logger.info('Prosess Start!!')
+
+file_path = "./q3/result/"
+
+step_2year = 2848
+step_1year = 1424
+l96 = Model_L96(N, F, dt, std)
+plot = Plot_Methods()
 
 
-logger.info('Prosess Start!!')                                                          # local parameter
-step_2year = 2848                                                                       # 4*356*2
-step_1year = 1424                                                                       # 4*356
-n_step_true = 4000                                                                      # process 2
-n_step_forcast_init = 1000                                                              # process 3, 4
-n_step_forcast = 201                                                                    # process 3, 4, 5, 400step - 100day
-
-for k in range(len(std_list)):
+# 1. L96を2年分シミュレーションする
+logger.info('Prosess 1')
+Xn_2year = np.zeros((N, step_2year))
+X1_2year = float(F)*np.ones(N)
+X1_2year[20] = 1.001*F
+Xn_2year, tn_2year = l96.analyze_model(Xn_2year, X1_2year, step_2year)
 
 
-    logger.info('Prosess 1')                                                            # 1. L96を2年分シミュレーションする
-    l96 = Model_L96(N, F, dt, std_list[k])
-    Xn_2year = np.zeros((N, step_2year))
-    X1_2year = float(F)*np.ones(N)
-    X1_2year[20] = 1.001*F
-    Xn_2year, tn_2year = l96.analyze_model(Xn_2year, X1_2year, step_2year)
+# 2. 後半1年分を6時間毎に保存する
+logger.info('Prosess 2')
+Xn_1year = np.zeros((N, step_1year))
+Xn_1year = Xn_2year[:, step_1year:step_2year]
 
-    
-    logger.info('Prosess 2')                                                            # 2. 後半1年分を6時間毎に保存する
-    Xn_1year = np.zeros((N, step_1year))
-    Xn_1year = Xn_2year[:, step_1year:step_2year]
-   
 
-    logger.info('Prosess 3')                                                            # 3. 真値の1-1000stepをそれぞれ初期値とし,　そこにノイズを加え、時間経過
-   
+# 3. Metsenne Twister法で乱数を生成する
+logger.info('Prosess 3')
+noise = np.random.normal(loc=mu, scale=sigma, size=step_2year)
+noise = list(map(lambda val: val*std, noise))
+#plot.graph_noise(noise, file_path)
+
+
+# 4. 真値にノイズを付加する
+logger.info('Prosess 4')
+Xn_forcast = np.zeros((N, step_1year))
+for i in range(step_1year):
+    Xn_forcast[:, i] = list(map(lambda val1, val2: val1 + val2, Xn_1year[:, i], noise))
+
 
 
 logger.info('Prosess finish')
