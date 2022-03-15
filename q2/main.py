9f0c45d8691dcd725072efd5d947d4fc4617e6d4
takaya <s.takaya@librus.jp>
2022-03-15 19:09:04 +0900

    q2、logも完成

--- q2/main.py
+++ q2/main.py
@@ -15,75 +15,66 @@ logger.warning('warning')
 logger.error('error')
 logger.info('Process End!')
 '''
-# logの初期設定
-logger = getLogger(__name__)
+
+logger = getLogger(__name__)                                                            # logの初期設定
 logger.setLevel(DEBUG)
 basicConfig(filename='console.log', level=DEBUG, filemode='w')
 
 
-# パラメータ設定
-N = 41
+N = 41                                                                                  # public parameter
 F = 8.0
 dt = 0.05
-std_list = [0.001, 0.01, 0.1, 1.0]
+std_list = [0.001]
 
-logger.info('Prosess Start!!')
+
+logger.info('Prosess Start!!')                                                          # local parameter
+n_step_init = 5000                                                                      # process 1
+n_step_true = 4000                                                                      # process 2
+n_step_forcast_init = 1000                                                              # process 3, 4
+n_step_forcast = 201                                                                    # process 3, 4, 5, 400step - 100day
+
 for k in range(len(std_list)):
-    # 3年分のシミュレーションを行い、真値の初期値を作成する。
-    logger.info('Prosess 1')
+
+
+    logger.info('Prosess 1')                                                            # 1. 3年分のシミュレーションを行い、真値の初期値を作成する。
     l96 = Model_L96(N, F, dt, std_list[k])
-
-    n_step_init = 5000 
     Xn_init = np.zeros((N, n_step_init))
-    X1_init= float(F)*np.ones(N)
+    X1_init = float(F)*np.ones(N)
     X1_init[20] = 1.001*F
+    Xn_init, tn_init = l96.analyze_model(Xn_init, X1_init, n_step_init)
 
-    Xn_init, tn_init= l96.analyze_model(Xn_init, X1_init, n_step_init)
-
-
-    # 初期値Xnをもとに真値Xn[40, :]を取得
-    logger.info('Prosess 2')
-
-    n_step_true = 4000
+    
+    logger.info('Prosess 2')                                                            # 2. 初期値Xnをもとに真値Xn[40, :]を取得
     Xn_true = np.zeros((N, n_step_true))
     X1_true = Xn_init[:, n_step_init-1]
-
     Xn_true, tn_true = l96.analyze_model(Xn_true, X1_true, n_step_true)
 
-
-    # 真値の1-1000stepをそれぞれ初期値とし,　そこにノイズを加え、時間経過
-    logger.info('Prosess 3')
-
-    n_step_forcast_init = 1000
-    n_step_forcast = 200 # 400step - 100day
+    
+    logger.info('Prosess 3')                                                            # 3. 真値の1-1000stepをそれぞれ初期値とし,　そこにノイズを加え、時間経過
     Xn_forcasts = []
     for i in range(n_step_forcast_init):
-        Xn_forcast = np.zeros((N, n_step_forcast))
         X1_forcast = l96.add_noise(Xn_true[:, i])
-
+        Xn_forcast = np.zeros((N, n_step_forcast))
         Xn_forcast, tn_forcast = l96.analyze_model(Xn_forcast, X1_forcast, n_step_forcast)
         Xn_forcasts.append(Xn_forcast)
 
 
-    # Xn_truesをスライス
-    logger.info('Prosess 4')
+    logger.info('Prosess 4')                                                            # 4. Xn_truesをスライス
     Xn_trues = []
     for i in range(n_step_forcast_init):
         Xn_trues.append(Xn_true[:, i:i+n_step_forcast])
 
-
-    # rmseを導出する。
-    logger.info('Prosess 5')
-    num_list = np.arange(0, 1000, step=1)
-    rmse_sum = [0] * 1000
-
-    for i in range(len(num_list)):
-        # すべてのシミュレーション結果の合計を求める
-        rmse = l96.get_RMSE_solo(Xn_trues, Xn_forcasts, n_step_forcast, num_list[i])
+    
+    logger.info('Prosess 5')                                                            # 5. rmseを導出する。
+    num_list = np.arange(0, n_step_forcast_init, step=1)                                # 全シミュレーションの添え字を作成して格納
+    rmse_sum = [0] * n_step_forcast_init                                                # RMSEの結果を保持するための配列
+    for i in range(n_step_forcast_init):                                                # すべてのシミュレーション結果の合計を求める
+        rmse = l96.get_RMSE_solo(Xn_trues, Xn_forcasts, n_step_forcast, num_list[i]) 
         rmse_sum = list(map(lambda val1, val2: val1 + val2, rmse_sum, rmse))
+    rmse_sum = list(map(lambda val: val / n_step_forcast_init, rmse_sum))                     # nで割って平均を求める
 
-    # nで割って平均を求める
-    rmse_sum = list(map(lambda val: val / len(num_list), rmse_sum))
+
+    '''
     logger.debug("---------")
     logger.debug(std_list[k])
     logger.debug(rmse_sum[0])
@@ -91,14 +82,14 @@ for k in range(len(std_list)):
     logger.debug(rmse_sum[4])
     logger.debug(rmse_sum[7])
     logger.debug(rmse_sum[8])
+    '''
 
-
-    # グラフ出力
-    logger.info('Prosess 6')
+    
+    logger.info('Prosess 6')                                                            # 6. グラフ出力
     file_path = "./q2/result/"
     plot = Plot_Methods()
-    #plot.xy_graph_rmse(rmse_sum, file_path, tn_forcast, std_list[k])
-    plot.xy_graph_rmse_log(rmse_sum, file_path, tn_forcast, std_list[k])
+    #plot.xy_graph_rmse(rmse_sum, file_path, tn_forcast, std_list[k])　                 # rmseのみの出力
+    plot.xy_graph_rmse_log(rmse_sum, file_path, tn_forcast, std_list[k])                # log scaleも出力
 
 
 logger.info('Prosess finish')
