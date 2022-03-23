3b157b714ed941cb6d867c11a9951795a02f17d5
takaya <s.takaya@librus.jp>
2022-03-23 16:58:35 +0900

    funcOfTimeの各δにおける時間経過のplotに成功

--- q4/main.py
+++ q4/main.py
@@ -44,6 +44,9 @@ filePath = "./q4/result/"
 step_2year = 2848
 step_t = 1424
 d = [0.00, 0.03, 0.05]
+step=[1, 10, 10]
+start = [0, 0, 250]
+end = [5, 50, 300]
 l96 = Model_L96(N, F, dt, delta)
 plot = Plot_Methods()
 
@@ -67,31 +70,32 @@ Y = np.zeros((N, step_t))
 for i in range(step_t):
     Y[:, i] = Xt[:, i] + np.random.normal(loc=mu, scale=sigma, size=N)
 
-for j in range(len(d)):
-    # 4. Kalman Filter
-    logger.info('Prosess 4')
-    Xf, Pf, Xa, Pa = l96.KF(Y, d[j])
+for i in range(len(d)):
+    for j in range(len(d)):
+        # 4. Kalman Filter
+        logger.info('Prosess 4')
+        Xf, Pf, Xa, Pa = l96.KF(Y, d[i])
+        fileName = "funcOfTime-" +  str(d[i]) + "-" + str(start[j]) + "-" + str(end[j]) + ".png"
+        XLabel = "time(day)"
+        YLabel = "X"
+        Title = "EKF, funcOfTime"
+        data = [Xt, Y, Xf, Xa, t_2year[0:step_t]]
+        params = [start[j], end[j]+1, step[j]]
+        names = [filePath, fileName, XLabel, YLabel, Title]
+        logger.debug('--- params---')
+        logger.debug(params)
 
-    start = 0
-    end = start + 50
-    fileName = "funcOfTime-" +  str(d[j]) + "-" + str(start) + "-" + str(end) + ".png"
-    XLabel = "time(day)"
-    YLabel = "X"
-    Title = "EKF, funcOfTime"
-    data = [Xt, Y, Xf, Xa, t_2year[0:step_t]]
-    params = [start, end+1]
-    names = [filePath, fileName, XLabel, YLabel, Title]
-
-    plot.funcOfTime(data, params, names)
+        plot.funcOfTime(data, params, names)
 
     # 5. RMSE & Spread
+    '''
     logger.info('Prosess 5')
     Xa_RMSE = l96.RMSE(Xa, Xt, step_t)
     Pa_Spread = l96.Spread(Pa)
 
     start = 0
     end = start + 175
-    fileName = "varianceInfration-" + str(d[j]) + "-" + str(start) + "-" + str(end) + ".png"
+    fileName = "varianceInfration-" + str(d[i]) + "-" + str(start) + "-" + str(end) + ".png"
     XLabel = "time(day)"
     YLabel = "X"
     Title = "EKF, VarianceInfration"
@@ -99,5 +103,6 @@ for j in range(len(d)):
     params = [start, end+1]
     names = [filePath, fileName, XLabel, YLabel, Title]
     plot.VarianceInfration(data, params, names)
+    '''
 
 logger.info('Prosess finish!!')
