8613dbc26c0f459196e6c2ea167e30eb39ad1586
takaya <s.takaya@librus.jp>
2022-04-12 14:52:48 +0900

    [fix] EnKF_OP 1が間違っていたので修正

--- q6/model_l96.py
+++ q6/model_l96.py
@@ -63,44 +63,54 @@ class Model_L96:
         # init setting
         step = len(Y[0])
         m_len = len(m)
-        Xb_sum = np.zeros((self.N, step))
-        Xb_mean = np.zeros((self.N, step))
         H = np.identity(self.N)
         R = np.identity(self.N)
         I = np.identity(self.N)
         M = np.zeros((self.N, self.N))
-        Xbs = []
-        Pbs = []
-        Xas = []
-        Pas = []
-
-        for i in range(m_len):
-            Xbs.append(np.zeros((self.N, step)))
-            Pbs.append(np.zeros(((self.N, self.N, step))))
-            Xas.append(np.zeros((self.N, step)))
-            Pas.append(np.zeros(((self.N, self.N, step))))
-
-
-        # 初期値代入
-        for i in range(m_len):
-            Xbs[i][:, 0] = Y[:, m[i]]
-            Pbs[i][:, :, 0] = np.diag([m[i]]*self.N)
-            Xas[i][:, 0] = Y[:, m[i]]
-            Pas[i][:, :, 0] = np.diag([m[i]]*self.N)
-        Xb_sum = Xas[i]
-
-        # 1. Ensemble Prediction (state)
-        for i in range(m_len):
-            for t in range(1, step):
-                Xbs[i][:, t] = self.RK4(Xas[i][:, t-1])
-
-            Xb_sum[:, :] += Xbs[i][:, :]
-        Xb_mean = Xb_sum / m_len
-
-        for i in range(m_len):
-            Xbs[i] -= Xb_mean
+        Xbs = np.zeros(((m_len, self.N, step)))
+        Pbs = np.zeros((((m_len, self.N, self.N, step))))
+        Xas = np.zeros(((m_len, self.N, step)))
+        Pas = np.zeros((((m_len, self.N, self.N, step))))
         
-        Zb = Xbs / np.sqrt(m_len-1)
+
+
+        # 1-1 Analysis Ensemble
+        for i in range(m_len):
+            Xbs[i, :, 0] = Y[:, m[i]]
+            Pbs[i, :, :, 0] = np.diag([m[i]]*self.N)
+            Xas[i, :, 0]  = Y[:, m[i]]
+            Pas[i, :, :, 0] = np.diag([m[i]]*self.N)
+
+        for t in range(1, step):
+            Xb_sum = np.zeros(self.N)
+            Xb_mean = np.zeros(self.N)
+            
+            for i in range(m_len):
+                # 1-2 Ensemble Forecasts, 時刻tにおける(40,)がm個
+                Xbs[i, :, t] = self.RK4(Xas[i, :, t-1])
+                Xb_sum += Xbs[i, :, t]
+    
+            # 1-3 Ensemble Mean, m個の平均, (40,)
+            Xb_mean = Xb_sum / m_len
+
+            # 1-4 Ensemble Perturbation, (m, 40)
+            dXbs = np.zeros((m_len, self.N))
+           
+            for i in range(m_len):
+                dXbs[i, :] = Xbs[i, :, t] - Xb_mean
+                #logger.debug('dXbs[i, :]={}'.format(dXbs[i, :]))
+            
+            logger.debug('m_len={}'.format(m_len))
+            logger.debug('Xb_mean.shape={}'.format(Xb_mean.shape))
+            logger.debug('dXbs.shape={}'.format(dXbs.shape))
+
+            # 2 Prediction of Error Covariance (implicitly)
+            Zb = dXbs / np.sqrt(m_len-1)
+            tmp = Zb@Zb.T
+            logger.debug('tmp.shape={}'.format(tmp.shape))
+        
+
+        
 
         # 2. Prediction of Error Covariance (implicitly)
         # 3. Kalman Gain
